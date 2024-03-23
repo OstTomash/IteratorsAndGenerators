ae263ad8aa939d7d0181c7118b422dfff6682252
Ostap Tomashivskyi <o.tomash650416@gmail.com>
2024-03-23 20:25:13 +0200

    Added method peak

---
+++ CycleIteratorPeak/main.py
@@ -0,0 +1,45 @@
+class CyclicIterator:
+    """
+    An iterator that cyclically iterates over a given collection indefinitely.
+
+    Args:
+    collection (iterable): The iterable collection to iterate over cyclically.
+    """
+
+    def __init__(self, collection):
+        """
+        Initializes the CyclicIterator with the given collection.
+
+        Args:
+        collection (iterable): The iterable collection to iterate over cyclically.
+        """
+        self.values = collection
+        self.index = 0
+
+    def __iter__(self):
+        """
+        Returns the iterator object itself.
+        """
+        return self
+
+    def peak(self):
+        """
+        Returns current element in the cyclic iteration
+        """
+        return self.values[self.index]
+
+    def __next__(self):
+        """
+        Returns the next element in the cyclic iteration.
+        """
+        if self.index >= len(self.values):
+            self.index = 0
+        result = self.values[self.index]
+        self.index += 1
+        return result
+
+
+cycle_iter = CyclicIterator([1, 2, 3])
+print(next(cycle_iter))
+print(cycle_iter.peak())
+print(next(cycle_iter))
